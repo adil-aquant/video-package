108e3f9bcb4fddc3a7c6895a7ea2911f88c67225
adil-aquant <adil.mir@netsmartz.net>
2022-01-20 15:52:27 +0530

    Added Option to Call

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import { multiply } from 'video-package';
+import { loadCallWithId } from 'video-package';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
@@ -12,9 +13,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+      <Button
+        title='Call'
+        onPress={() => loadCallWithId('rya8PwW1',true) } 
+      />
+   </View>
+);
 }
 
 const styles = StyleSheet.create({
